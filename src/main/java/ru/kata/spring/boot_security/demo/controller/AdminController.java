ccf04c108641185c99c8c8f87a179a9c31dd448f
Anastasia Dubrovina <lukinova.anastasiya@mail.ru>
2023-04-27 23:41:29 +0300

    JS

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -1,14 +1,24 @@
 package ru.kata.spring.boot_security.demo.controller;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.net.URI;
+import java.util.List;
 
 
 @Controller
@@ -21,7 +31,6 @@ public class AdminController {
     }
 
 
-
     @GetMapping("/admin")
     public String allUsers(Model model) {
         model.addAttribute("users", userService.getAllUsers());
@@ -32,39 +41,43 @@ public class AdminController {
         return "adminPanel";
     }
 
-    @GetMapping("/admin/user/{id}")
+
+    @GetMapping("/user/{id}")
     public String showUser(@PathVariable("id") Long id, Model model) {
         model.addAttribute("user", userService.showById(id));
         return "userPage";
     }
 
-    @GetMapping("/admin/new")
+
+    @GetMapping("/new")
     public String newUser(Model model) {
         model.addAttribute("userNew", new User());
         model.addAttribute("allRoles", userService.findAllRoles());
         return "adminPanel";
     }
 
-    @PostMapping("/admin/user")
+
+    @PostMapping("/user")
     public String createUser(User user) {
         userService.saveUser(user);
         return "redirect:/admin";
     }
 
-    @GetMapping("/admin/user/{id}/edit")
+    @GetMapping("/user/{id}/edit")
     public String edit(Model model, @PathVariable("id") Long id) {
         model.addAttribute("user", userService.showById(id));
         model.addAttribute("allRoles", userService.findAllRoles());
         return "adminPanel";
     }
 
-    @PatchMapping("/admin/user/{id}")
+    @PatchMapping("/user/{id}")
     public String update(User user, @PathVariable("id") Long id, Model model) {
         userService.updateUserById(id, user, model);
         return "redirect:/admin";
     }
 
-    @DeleteMapping("/admin/user/{id}")
+
+    @DeleteMapping("/user/{id}")
     public String delete(@PathVariable("id") Long id) {
         userService.removeUserById(id);
         return "redirect:/admin";
--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -1,15 +1,26 @@
 package ru.kata.spring.boot_security.demo.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
+
+import java.security.Principal;
+
 @Controller
+@RequestMapping("/user")
 public class UserController {
     private UserService userService;
 
@@ -18,12 +29,51 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/user")
+//    //для теста постман
+//    @GetMapping()
+//    public ResponseEntity<User> showUser( Model model) throws JsonProcessingException {
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        User userCont = (User) authentication.getPrincipal();
+//        model.addAttribute("user", userCont);
+////        User user = userService.showById(id);
+////        String userJson = new ObjectMapper().writeValueAsString(userCont);
+//        return ResponseEntity.ok(userCont);
+//    }
+
+
+//старый
+    @GetMapping()
     public String showUserInfo(Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         User user = (User) authentication.getPrincipal();
         model.addAttribute("user", user);
         return "userPage";
-    }
 
+    }
+    // предполагаемый
+//    @GetMapping("/user")
+//    public ResponseEntity<String> showUserInfo(Model model) throws JsonProcessingException {
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        User user = (User) authentication.getPrincipal();
+//        model.addAttribute("user", user);
+//        String userJson = new ObjectMapper().writeValueAsString(user);
+////        return "userPage";
+//        return ResponseEntity.ok(userJson);
+//    }
+
+
+
+
+    /////два метода для примера
+//    @GetMapping("/user")
+//    public ResponseEntity<User> getUser(Principal principal) {
+//        return new ResponseEntity<>(convertToUserDto(userService.findByUsername(principal.getName())), HttpStatus.OK);
+//    }
+//
+//    public User convertToUserDto(User user) {
+//
+//        ModelMapper modelMapper = new ModelMapper();
+//        return modelMapper.map(user, User.class);
+//
+//    }
 }
